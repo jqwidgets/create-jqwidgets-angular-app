928eed5e76f0d1ca0399f9fb0a52531ac15b2b42
jQWidgets <info@jqwidgets.com>
2017-11-09 16:24:42 +0200

    Updated to Angular 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 ﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { jqxGridComponent } from 'jqwidgets-framework/jqwidgets-ts/angular_jqxgrid';
+import { jqxGridComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxgrid';
 
 import { AppComponent } from './app.component';
 
