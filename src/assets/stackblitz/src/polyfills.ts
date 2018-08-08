f3f082aadf0d78cfae4c7b7d356f9aa6c29a4537
jQWidgets <info@jqwidgets.com>
2018-08-08 13:06:50 +0300

    Delete polyfills.ts

--- src/assets/stackblitz/src/polyfills.ts
+++
@@ -1,9 +0,0 @@
-/**
-    * This file includes polyfills needed by Angular and is loaded before the app.
-    * You can add your own extra polyfills to this file.
-    *
-**/
-
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
